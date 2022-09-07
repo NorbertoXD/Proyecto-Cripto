e5e802978b1fa93e2ca5a631ca592fabbf215605
Norberto <norbertoalves22@hotmail.com>
2022-09-07 22:43:41 +0200

    creando ventanas

--- balance/views.py
+++ balance/views.py
@@ -1,11 +1,19 @@
-from flask import render_template
+#from flask import render_template
 
-from . import app
-from .models import ListaMovimientos
+from balance import app
+#from .models import ListaMovimientos
 
 
 @app.route('/')
-def inicio():
-    movimientos = ListaMovimientos()
-    movimientos.leer_archivo()
-    
+def home():
+    #movimientos = ListaMovimientos()
+    #movimientos.leer_archivo()
+    return "INICIOOOO"
+
+@app.route('/purchase')
+def compra():
+    return "COMPRA"
+
+@app.route('/status')
+def estado():
+    return "ESTADO"
