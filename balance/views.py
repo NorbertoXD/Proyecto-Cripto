7b5dd69abff73250682f060de492b0c512377ca9
Norberto <norbertoalves22@hotmail.com>
2022-09-18 20:37:39 +0200

    ya se ve la base de datos

--- balance/views.py
+++ balance/views.py
@@ -1,21 +1,28 @@
-from flask import render_template
+from flask import render_template, request
 
 from . import app
-from .models import ListaMovimientos
+from .models import DBManager
+
+RUTA = 'data/balance.db'
 
 
-
-@app.route('/')
+@app.route("/")
 def home():
-    movimientos = ListaMovimientos()
-    movimientos.leer_archivo()
-    return render_template("inicio.html", movs=movimientos.movimientos)
+    db = DBManager(RUTA)
+    movimientos = db.consultaSQL("SELECT * FROM movimientos")
+    return render_template("inicio.html", movs=movimientos)
 
 
-@app.route('/purchase')
+
+@app.route("/purchase", methods=["GET", "POST"])
 def compra():
-    return render_template("purchase.html")
+    if request.method == "GET":
+        return render_template("purchase.html")
+    #else:
+        #¿return " he llegado aqui gracias a post"
 
-@app.route('/status')
+
+
+@app.route("/status", methods=["GET", "POST"])
 def estado():
     return render_template("status.html")
