dbf6cf959b388e478f7fae5fe5c25d656deef053
Norberto <norbertoalves22@hotmail.com>
2022-09-20 23:16:28 +0200

    avance

--- balance/forms.py
+++ balance/forms.py
@@ -1,35 +1,32 @@
 from flask_wtf import FlaskForm
-from wtforms import HiddenField, SubmitField, SelectField, FloatField
+from datetime import date, datetime
+from wtforms import HiddenField, SubmitField, DateField, TimeField, SelectField, FloatField
 from wtforms.validators import DataRequired
 
 
 class Movimientosform(FlaskForm):
     id = HiddenField()
-    fecha = HiddenField()
-    hora = HiddenField()
+    fecha = DateField()
+    hora = TimeField()
+   
     moneda_from = SelectField("Moneda_from: ", choices=[(
         "USD", "USD"), ("BTC", "BTC"), ("BNB", "BNB"), ("ETH", "ETH"), ("EUR", "EUR")],
-        validators=[DataRequired(message="Tienes que seleccionar la moneda a vender")])
+        validators=[DataRequired(message="Tienes que seleccionar la moneda_from")])
 
-    cantidad_from = FloatField(
-        "Moneda_from: ", validators=
-        [DataRequired(message="Cantidad de venta")])
 
-    
     moneda_to = SelectField("Moneda_to: ", choices=[(
         "USD", "USD"), ("BTC", "BTC"), ("BNB", "BNB"), ("ETH", "ETH"), ("EUR", "EUR")],
-        validators=[DataRequired(message="Tienes que seleccionar la moneda to")])
+        validators=[DataRequired(message="Tienes que seleccionar la moneda_to")])
 
 
-    cantidad_to = FloatField(
-        "Moneda_from: ", validators=
+    cantidad_from = FloatField(
+        "cantidad_from: ", validators=
         [DataRequired(message="Cantidad de venta")])
 
-    consulta_api = SubmitField("Consulta")
-
-
-    cancelar = SubmitField(
-        '✖', render_kw={'class':'green-button'})
     
     
-    guardar = SubmitField("✔", render_kw={"class":"green-button"})
+    consultarapi = SubmitField("ConsultarAPI")
+    
+    enviar = SubmitField("Guardar")
+    
+    borrar = SubmitField("Borrar")
